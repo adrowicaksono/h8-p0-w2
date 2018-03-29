cf4a66c4a6201b28b2a17adc2e6ed390e741c2b4
adrowicaksono <adrowicaksono@gmail.com>
2018-03-29 10:19:16 +0700

    fixing switch case statement(bulan)

--- switch-case.js
+++ switch-case.js
@@ -1,30 +1,29 @@
 var tanggal = 8;
-var bulan = 5;
+var bulan = 8;
 var tahun= 1900;
 var tampil = '';
 
+  
+
 
   if (tanggal > 0 && tanggal < 32 && tahun > 1899 && tahun < 2201 && bulan > 0 && bulan < 13 ){
-       
-      switch(bulan){
-        case 1  : tampil = tanggal + ' januari ' + tahun; break;
-        case 2  : tampil = tanggal + ' februari ' + tahun; break;
-        case 3  : tampil = tanggal + ' maret ' + tahun; break;
-        case 4  : tampil = tanggal + ' april ' + tahun; break;
-        case 5  : tampil = tanggal + ' mei ' + tahun; break;
-        case 6  : tampil = tanggal + ' juni ' + tahun; break;
-        case 7  : tampil = tanggal + ' juli ' + tahun; break;
-        case 8  : tampil = tanggal + ' agustus ' + tahun; break;
-        case 9  : tampil = tanggal + ' september ' + tahun; break;
-        case 10 : tampil = tanggal + ' oktober ' + tahun; break;
-        case 11 : tampil = tanggal + ' november ' + tahun; break;
-        case 12 : tampil = tanggal + ' desember ' + tahun; break;
-        
-      } 
+     
+    switch(bulan){
+      case 1 : bulan = 'januari'; break;
+      case 2 : bulan = 'februari'; break;
+      case 3 : bulan = 'maret'; break;
+      case 4 : bulan = 'april'; break;
+      case 5 : bulan = 'mei'; break;
+      case 6 : bulan = 'juni'; break;
+      case 7 : bulan = 'juli'; break;
+      case 8 : bulan = 'agustus'; break;
+      case 9 : bulan = 'september'; break;
+      case 10 : bulan = 'oktober'; break;
+      case 11 : bulan = 'november'; break;
+      case 12 : bulan = 'desember'; break;
+    }
       
-      
-      
-    console.log(tampil);
+    console.log(tanggal+' ' + bulan +' '+ tahun);
     
   }
    else console.log('tanggal tidak tersedia')
